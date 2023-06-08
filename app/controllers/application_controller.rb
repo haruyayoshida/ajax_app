ef300b3f1d6a761a15187c2cb6485a4959399454
haruyayoshida <haruyayoshida06@gmail.com>
2023-06-08 21:56:23 +0900

    ユーザー認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basi_auth
+
+  private
+
+  def basi_auth
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
